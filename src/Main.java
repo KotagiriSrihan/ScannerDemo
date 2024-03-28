715d4dec0a0b50183f848a68b1c6d267d8da89cc
Kotagiri Srihan <kotagirisrihan@gmail.com>
2024-03-28 07:27:53 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+//        int a=909;
+//        int n,n1,n2;
+//        n=a%10;
+//        System.out.println(n);
+//        a=a/10;
+//        n1=a%10;
+//        System.out.println(n1);
+//        a=a/10;
+//        n2=a%10;
+//        System.out.println(n2);
+
+//        int b=345;
+//        int n,n1,n2;
+//        n=b%10;
+//        System.out.println(n);
+//        b=b/10;
+//        n1=b%10;
+//        System.out.println(n1);
+//        b=b/10;
+//        n2=b%10;
+//        System.out.println(n2);
+//        System.out.println("Reverse of number is: "+n+n1+n2);
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age=scanner.nextInt();
+        System.out.println("Enter your name: ");
+        String name=scanner.next();
+        System.out.println("NAME: "+name);
+        System.out.println("Age :"+age);
+
+        
+
+
+    }
+}
